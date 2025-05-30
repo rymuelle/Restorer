4f80538cb560fb6d7967ca5d7de83f6ac0a92f5d
Ryan Mueller <ryan.dalrymple.mueller@gmail.com>
2025-05-30 15:25:22 -0400

    Updated loading and running test.

--- tests/load_and_run.py
+++
@@ -1,8 +0,0 @@
-import torch
-from ConvNeXtUNet.convnextv2_unet import convnextv2unet_atto
-
-model = convnextv2unet_atto()
-
-test_data = torch.rand(1, 3, 32, 32)
-
-output = model(test_data)
---
+++ tests/test_load_and_run.py
@@ -0,0 +1,23 @@
+import pytest
+import torch
+from ConvNeXtUNet.convnextv2_unet import convnextv2unet_atto
+
+def test_main_model_loads():
+    """
+    Test that the model can be loaded.
+    """
+    print("\nRunning test_main_model_loads...")
+    model = convnextv2unet_atto()
+    assert isinstance(model, convnextv2unet_atto)
+    print("Model loaded.")
+
+def test_model_prediction_with_valid_data():
+    """
+    Test that the model can process valid random data.
+    """
+    print("\nRunning test_model_prediction_with_valid_data...")
+    model = convnextv2unet_atto() 
+    test_data = torch.rand(1, 3, 32, 32)
+    result = model.predict(test_data)
+    assert result.shape == test_data.shape
+    print(f"Prediction test passed.")
