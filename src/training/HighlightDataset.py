de283ca2f9fde20c23a7137c0f7341147bf87003
Ryan Mueller <33radar@gmail.com>
2026-05-23 12:46:10 -0500

    Highlight reconstruction first attempt

---
+++ src/training/HighlightDataset.py
@@ -0,0 +1,124 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+import numpy as np
+import cv2
+
+def random_crop_dim(shape, crop_size, buffer, validation=False):
+        h, w = shape
+        if not validation:
+            top = np.random.randint(0 + buffer, h - crop_size - buffer)
+            left = np.random.randint(0 + buffer, w - crop_size - buffer)
+        else:
+            top = (h - crop_size) // 2
+            left = (w - crop_size) // 2
+
+        bottom = top + crop_size
+        right = left + crop_size
+        return (left, right, top, bottom)
+
+patterns = [np.array([[0., 2., 1., 2., 0., 1.],
+        [1., 1., 0., 1., 1., 2.],
+        [1., 1., 2., 1., 1., 0.],
+        [2., 0., 1., 0., 2., 1.],
+        [1., 1., 2., 1., 1., 0.],
+        [1., 1., 0., 1., 1., 2.]], dtype=np.float16),
+ np.array([[0., 1.],
+        [3., 2.]], dtype=np.float16),
+ np.array([[3., 2.],
+        [0., 1.]], dtype=np.float16),
+ np.array([[1., 1., 0., 1., 1., 2.],
+        [1., 1., 2., 1., 1., 0.],
+        [2., 0., 1., 0., 2., 1.],
+        [1., 1., 2., 1., 1., 0.],
+        [1., 1., 0., 1., 1., 2.],
+        [0., 2., 1., 2., 0., 1.]], dtype=np.float16),
+ np.array([[2., 3.],
+        [1., 0.]], dtype=np.float16)]
+
+
+def compute_mask(shape, pattern):
+    H, W = shape
+    ph, pw = pattern.shape
+    # If two green channels, set both to 1
+    _pattern = pattern.copy()
+    _pattern[_pattern == 3] = 1
+    # Create the output arrays
+    mask = np.zeros((3, H, W), dtype=np.uint8)
+
+    # Tile the pattern to match the CFA shape
+    full_pattern = np.tile(_pattern, (H // ph + 1, W // pw + 1))
+    full_pattern = full_pattern[:H, :W]
+
+    # Vectorized assignment for each channel (R, G, B)
+    for ch in range(3):
+        ch_mask = full_pattern == ch
+        mask[ch] = ch_mask
+    return  mask
+
+def make_masks(size, patterns):
+    masks = []
+    for pattern in patterns:
+        mask = compute_mask(size, pattern)
+        masks.append( (pattern, mask))
+    return masks
+
+def get_mask(pattern, mask):
+    for _pattern, mask in mask:
+        if np.array_equal(pattern, _pattern): 
+            return mask
+    return 0
+
+class HighlightDataset(Dataset):
+    def __init__(self, csv, crop_size=256, buffer=10, validation=False, resize=1, clipping_threshold=0.8):
+        super().__init__()
+        self.csv = pd.read_csv(csv)
+        self.crop_size = crop_size
+        self.validation = validation 
+        self.buffer = buffer
+        self.masks = make_masks((crop_size+6, crop_size+6), patterns)
+        self.resize = resize
+        self.clipping_threshold = clipping_threshold
+    
+    def __len__(self):
+        return len(self.csv)
+    
+    def __getitem__(self, i):
+        row = self.csv.iloc[i]
+        gt_path = row["gt_out"]
+        deg_pattern_path = row["deg_out_ccm"].replace('ccm', 'pattern')
+        deg_ccm = np.load(row["deg_out_ccm"])
+
+        gt_image = np.load(gt_path, mmap_mode="r")
+        deg_pattern = np.load(deg_pattern_path)
+
+        H, W, C = gt_image.shape
+        dims = random_crop_dim((W, H), self.crop_size*self.resize, self.buffer, validation=self.validation)
+        h1, h2, w1, w2 = dims
+        gt_image = np.array(gt_image[h1:h2, w1:w2]).astype('float32')
+        if self.resize>1:
+            gt_image = cv2.resize(gt_image, (self.crop_size, self.crop_size), interpolation=cv2.INTER_AREA)
+        gt_image = gt_image.transpose(2, 0, 1)
+        gt_image *= 1/gt_image.max()
+        clipped_image = gt_image.clip(0, self.clipping_threshold)
+        clipping_mask =( gt_image-clipped_image)>0
+
+        mask = get_mask(deg_pattern, self.masks)
+        C, H, W = mask.shape
+        dims = random_crop_dim((W, H), self.crop_size, 0, validation=self.validation)
+        h1, h2, w1, w2 = dims
+        mask = mask[:, h1:h2, w1:w2]
+        six_chan = np.concat([gt_image*mask, mask], axis=0)
+        six_chan_clipped = np.concat([clipped_image*mask, mask], axis=0)
+
+        output = {
+            "gt": torch.from_numpy(gt_image).to(torch.float32).clamp_(0.0, 1.0),
+            "clipped_image": torch.from_numpy(clipped_image).to(torch.float32).clamp_(0.0, 1.0),
+            "clipping_mask": torch.from_numpy(clipping_mask),
+            "six_chan": torch.from_numpy(six_chan).to(torch.float32).clamp_(0.0, 1.0),
+            "six_chan_clipped": torch.from_numpy(six_chan_clipped).to(torch.float32).clamp_(0.0, 1.0),
+            "ccm": torch.from_numpy(deg_ccm).to(torch.float32)
+   
+        }
+        return output
---
+++ train_highlight.py
@@ -0,0 +1,176 @@
+import os
+import sys
+import torch
+import torch.nn as nn
+import torchvision.transforms as T
+from torch.utils.flop_counter import FlopCounterMode
+
+from torch.utils.data import DataLoader, random_split
+from tqdm import tqdm
+import mlflow
+import mlflow.pytorch
+from SWLoss.src.slicing_loss import SlicingLoss, GramLoss
+
+from pathlib import Path
+from src.training.load_config import load_config
+from src.training.losses.CCMLoss import CCMLoss
+
+# Read config
+run_config = load_config()
+outpath = Path(run_config['numpy_raw_subdir'])
+alignment_csv =  outpath / run_config['align_csv']
+from src.training.HighlightDataset import HighlightDataset
+from src.Restorer.DemoNAFNet import DemoNAFNet
+from src.training.losses.PSNR import PSNRLoss, psnr
+
+
+CONFIG = {
+    "model_name": "DemoNAf_highlight",
+    "experiment_name": "BaseHighlight",
+    "batch_size": 16,
+    "lr": 5e-4,
+    "sched_end_factor": 1e-1,
+    "epochs": 1200,
+    "seed": 42,
+    "num_workers": 16,
+    "device": "cuda" if torch.cuda.is_available() else "cpu",
+    "width": 32,
+    "middle_blk_num": 8,
+    "enc_blk_nums":[(0, 0), (0, 0), (4, 0), (4, 0)],
+    "dec_blk_nums":[(4, 0), (4, 0), (0, 0), (0, 0)],
+    "in_channels": 6,
+    "lumi_noise": 0,
+    "crop_size": 64+16,
+    "residual_mask": False,
+    'SWL_scale': 0.1,
+    "iso_range": [0, 1e9],
+    "added_noise": 0.,
+    "no_raf": True,
+    "iter_per_iter": 1,
+    "CSV": "refit.csv",
+    "gb_filter": .1,
+    "clipping_threshold": 0.5,
+
+}
+
+def measure_flops(model, mlflow):
+    flop_counter = FlopCounterMode(display=True)
+    with flop_counter:
+        model(torch.rand(1, 6, 256, 256).to(CONFIG['device']))
+    total_flops = flop_counter.get_total_flops()
+
+    mlflow.log_metric("total_flops", total_flops)
+        
+def train():
+    generator = torch.Generator().manual_seed(CONFIG["seed"])
+
+
+    # dataset = JDDDataset(alignment_csv, validation=False, crop_size=(CONFIG['crop_size']))
+    dataset = HighlightDataset(CONFIG['CSV'], validation=False, crop_size=CONFIG['crop_size'], 
+                               clipping_threshold=CONFIG['clipping_threshold'])
+    dataset.csv = dataset.csv[~dataset.csv.bad]
+    dataset.csv = dataset.csv[(dataset.csv.iso >= CONFIG['iso_range'][0]) & 
+                              (dataset.csv.iso <= CONFIG['iso_range'][1])]
+    dataset.csv = dataset.csv[(dataset.csv.gb-1).abs()<CONFIG['gb_filter']]
+
+    if CONFIG['no_raf']:
+        dataset.csv['raf'] =  dataset.csv.gt_out.str.contains('.raf')
+        dataset.csv = dataset.csv[~dataset.csv.raf]
+    dataset.csv.reset_index(inplace=True)
+
+    train_size = int(0.8 * len(dataset))
+    val_size = len(dataset) - train_size
+    train_set, val_set = random_split(dataset, [train_size, val_size], generator=generator)
+
+    train_loader = DataLoader(train_set, batch_size=CONFIG["batch_size"], shuffle=True, 
+                              generator=generator, num_workers=CONFIG["num_workers"])
+    val_loader = DataLoader(val_set, batch_size=CONFIG["batch_size"], shuffle=False, 
+                            generator=generator, num_workers=CONFIG["num_workers"])
+
+    model = DemoNAFNet(in_channels=CONFIG['in_channels'], width=CONFIG["width"],
+                        middle_blk_num=CONFIG["middle_blk_num"], 
+                   enc_blk_nums=CONFIG["enc_blk_nums"], dec_blk_nums=CONFIG["dec_blk_nums"],
+                     mask=CONFIG['residual_mask'],
+                    ).to(CONFIG["device"], )
+                   
+    optimizer = torch.optim.Adam(model.parameters(), lr=CONFIG["lr"])
+    scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=1.0, end_factor=CONFIG["sched_end_factor"], total_iters=CONFIG["epochs"])
+    criterion = nn.L1Loss()
+    # criterion = CCMLoss()
+    texture_criteria = SlicingLoss(1).to(CONFIG['device'])
+
+    # MLflow Tracking
+    mlflow.set_experiment(CONFIG["experiment_name"])
+
+    with mlflow.start_run(run_name=CONFIG["model_name"]):
+        # Log Hyperparameters
+        mlflow.log_params(CONFIG)
+        measure_flops(model, mlflow)
+        for epoch in range(CONFIG["epochs"]):
+            # Trainig
+            model.train()
+            train_loss = 0.0
+            tloader = tqdm(train_loader, desc=f"Epoch {epoch+1}/{CONFIG['epochs']} [Train]")
+            
+            for output in tloader:
+                images, sparse = output['gt'].to(CONFIG["device"]), output['six_chan_clipped'].to(CONFIG["device"])
+                ccm = output['ccm'].to(CONFIG["device"])
+                clipping_mask = output['clipping_mask'].to(CONFIG["device"])
+                for _ in range(CONFIG['iter_per_iter']):
+                    optimizer.zero_grad()
+                    with torch.autocast(device_type=CONFIG["device"], dtype=torch.bfloat16):
+                        output = model(sparse)
+                        if CONFIG['SWL_scale'] > 0:
+                            tloss = texture_criteria(output, images) * CONFIG['SWL_scale']
+                        else:
+                            tloss = 0 
+                        loss = criterion(output, images)
+                    loss += tloss
+                    loss.backward()
+                    optimizer.step()
+                    
+                    train_loss += loss.item() * images.size(0)
+                    tloader.set_postfix({"loss": f"{loss.item():.4e}"})
+            
+            avg_train_loss = train_loss / (len(train_set) * CONFIG['iter_per_iter'])
+            mlflow.log_metric("train_l1_loss", avg_train_loss, step=epoch)
+            scheduler.step()
+
+            # Validation
+            model.eval()
+            val_loss = 0.0
+            val_psnr_loss = 0.0
+            vloader = tqdm(val_loader, desc=f"Epoch {epoch+1} [Val]")
+            
+            with torch.no_grad():
+                for output in vloader:
+                    images, sparse = output['gt'].to(CONFIG["device"]), output['six_chan_clipped'].to(CONFIG["device"])
+                    ccm = output['ccm'].to(CONFIG["device"])
+                    clipping_mask = output['clipping_mask'].to(CONFIG["device"])
+                    output = model(sparse)
+                    loss = criterion(output, images)
+                    if CONFIG['SWL_scale'] > 0:
+                        tloss = texture_criteria(output, images) * CONFIG['SWL_scale']
+                    else:
+                        tloss = 0 
+                   
+                    loss += tloss
+                    val_loss += loss.item() * images.size(0)
+                    psnr_loss = psnr(output, images) * images.size(0)
+                    val_psnr_loss += psnr_loss
+            
+            avg_val_loss = val_loss / len(val_set)
+            avg_val_psnr = val_psnr_loss / len(val_set)
+            mlflow.log_metric("val_l1_loss", avg_val_loss, step=epoch)
+            mlflow.log_metric("val_psnr_loss", avg_val_psnr, step=epoch)
+            print(f"Epoch {epoch+1}: Val Loss: {avg_val_loss:.4e} PSNR: {avg_val_psnr:.1f}")
+
+        # Save Artifacts
+        mlflow.pytorch.log_model(model, "model")
+        
+        # Local save as backup
+        torch.save(model.state_dict(), f"{CONFIG['model_name']}_final.pth")
+
+if __name__ == "__main__":
+    torch.cuda.empty_cache()
+    train()
