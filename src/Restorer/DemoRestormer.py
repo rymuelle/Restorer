41478371599b28bfc030312879a6e14e1a133733
Ryan Mueller <33radar@gmail.com>
2026-06-01 10:56:14 -0500

    Modified Restormer variant

---
+++ src/Restorer/DemoRestormer.py
@@ -0,0 +1,196 @@
+# ------------------------------------------------------------------------
+# Copyright (c) 2022 megvii-model. All Rights Reserved.
+# ------------------------------------------------------------------------
+
+'''
+Simple Baselines for Image Restoration
+
+@article{chen2022simple,
+  title={Simple Baselines for Image Restoration},
+  author={Chen, Liangyu and Chu, Xiaojie and Zhang, Xiangyu and Sun, Jian},
+  journal={arXiv preprint arXiv:2204.04676},
+  year={2022}
+}
+'''
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class MDTA(nn.Module):
+    def __init__(self, channels, num_heads):
+        super(MDTA, self).__init__()
+        self.num_heads = num_heads
+        self.temperature = nn.Parameter(torch.ones(1, num_heads, 1, 1))
+
+        self.qkv = nn.Conv2d(channels, channels * 3, kernel_size=1, bias=False)
+        self.qkv_conv = nn.Conv2d(channels * 3, channels * 3, kernel_size=3, padding=1, groups=channels * 3, bias=False)
+        self.project_out = nn.Conv2d(channels, channels, kernel_size=1, bias=False)
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        q, k, v = self.qkv_conv(self.qkv(x)).chunk(3, dim=1)
+
+        q = q.reshape(b, self.num_heads, -1, h * w)
+        k = k.reshape(b, self.num_heads, -1, h * w)
+        v = v.reshape(b, self.num_heads, -1, h * w)
+        q, k = F.normalize(q, dim=-1), F.normalize(k, dim=-1)
+
+        attn = torch.softmax(torch.matmul(q, k.transpose(-2, -1).contiguous()) * self.temperature, dim=-1)
+        out = self.project_out(torch.matmul(attn, v).reshape(b, -1, h, w))
+        return out
+
+
+class GDFN(nn.Module):
+    def __init__(self, channels, expansion_factor):
+        super(GDFN, self).__init__()
+
+        hidden_channels = int(channels * expansion_factor)
+        self.project_in = nn.Conv2d(channels, hidden_channels * 2, kernel_size=1, bias=False)
+        self.conv = nn.Conv2d(hidden_channels * 2, hidden_channels * 2, kernel_size=3, padding=1,
+                              groups=hidden_channels * 2, bias=False)
+        self.project_out = nn.Conv2d(hidden_channels, channels, kernel_size=1, bias=False)
+
+    def forward(self, x):
+        x1, x2 = self.conv(self.project_in(x)).chunk(2, dim=1)
+        x = self.project_out(F.gelu(x1) * x2)
+        return x
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, channels, num_heads, expansion_factor):
+        super(TransformerBlock, self).__init__()
+        self.norm1 = nn.LayerNorm(channels)
+        self.attn = MDTA(channels, num_heads)
+        self.norm2 = nn.LayerNorm(channels)
+        self.ffn = GDFN(channels, expansion_factor)
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        x = x + self.attn(self.norm1(x.reshape(b, c, -1).transpose(-2, -1).contiguous()).transpose(-2, -1)
+                          .contiguous().reshape(b, c, h, w))
+        x = x + self.ffn(self.norm2(x.reshape(b, c, -1).transpose(-2, -1).contiguous()).transpose(-2, -1)
+                         .contiguous().reshape(b, c, h, w))
+        return x
+
+class SimpleBlock(nn.Module):
+    def __init__(self, c, FFN_Expand=2):
+        super().__init__()
+        self.sg = SimpleGate()
+        ffn_channel = FFN_Expand * c
+        self.conv1 = nn.Conv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.norm1 = nn.GroupNorm(1, c)
+        self.beta = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
+
+    def forward(self, inp):
+        x = inp
+        x = self.conv1(self.norm1(x))
+        x = self.sg(x)
+        return inp + x * self.beta
+
+
+
+class DemoRestormer(nn.Module):
+
+    def __init__(self, img_channel=3, in_channels=6, width=16, 
+                 middle_blk_num=1, enc_blk_nums=[], dec_blk_nums=[], num_heads=[], mask=True,
+                 residual=False):
+        super().__init__()
+ 
+        # img_channel = in_channels // 2
+        self.img_channel = img_channel
+        if in_channels is None:
+            in_channels = img_channel
+        self.mask = mask
+        self.residual = residual
+
+        self.intro = nn.Conv2d(in_channels=in_channels, out_channels=width, kernel_size=3, padding=1, stride=1, groups=1,
+                              bias=True)
+ 
+        self.ending = nn.Conv2d(in_channels=width, out_channels=img_channel, kernel_size=3, padding=1, stride=1, groups=1,
+                              bias=True)
+
+        self.encoders = nn.ModuleList()
+        self.decoders = nn.ModuleList()
+        self.middle_blks = nn.ModuleList()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+
+        chan = width
+        for i, (num, snum) in enumerate(enc_blk_nums):
+            num_head = num_heads[i]
+            self.encoders.append(
+                nn.Sequential(
+                    *[TransformerBlock(chan, num_head, 2) for _ in range(num)],
+                    *[SimpleBlock(chan) for _ in range(snum)]
+                )
+            )
+            self.downs.append(
+                nn.Conv2d(chan, 2*chan, 2, 2)
+            )
+            chan = chan * 2
+
+        self.middle_blks = \
+            nn.Sequential(
+                *[TransformerBlock(chan, num_heads[-1], 2) for _ in range(middle_blk_num[0])]
+            )
+
+        for i, (num, snum) in enumerate(dec_blk_nums):
+            num_head = num_heads[-1-i]
+            self.ups.append(
+                nn.Sequential(
+                    nn.Conv2d(chan, chan * 2, 1, bias=False),
+                    nn.PixelShuffle(2)
+                )
+            )
+            chan = chan // 2
+            self.decoders.append(
+                nn.Sequential(
+                    *[TransformerBlock(chan, num_head, 2) for _ in range(num)],
+                    *[SimpleBlock(chan) for _ in range(snum)]
+                )
+            )
+
+        self.padder_size = 2 ** len(self.encoders)
+
+    def forward(self, inp):
+        B, C, H, W = inp.shape
+        inp = self.check_image_size(inp)
+
+        x = self.intro(inp)
+
+        encs = []
+
+        for encoder, down in zip(self.encoders, self.downs):
+            x = encoder(x)
+            encs.append(x)
+            x = down(x)
+
+        x = self.middle_blks(x)
+
+        for decoder, up, enc_skip in zip(self.decoders, self.ups, encs[::-1]):
+            x = up(x)
+            x = x + enc_skip
+            x = decoder(x)
+        x = self.ending(x)
+        x = x[:, :, :H, :W]
+
+        if self.mask:
+            sparse, mask = inp.chunk(2, dim=1)
+            x = x * (mask==0) + sparse
+
+        if self.residual:
+            _, c, _, _ = x.shape
+            x = x + inp[:,:c]
+        return x
+
+    def check_image_size(self, x):
+        _, _, h, w = x.size()
+        mod_pad_h = (self.padder_size - h % self.padder_size) % self.padder_size
+        mod_pad_w = (self.padder_size - w % self.padder_size) % self.padder_size
+        x = F.pad(x, (0, mod_pad_w, 0, mod_pad_h))
+        return x
+
+
+
